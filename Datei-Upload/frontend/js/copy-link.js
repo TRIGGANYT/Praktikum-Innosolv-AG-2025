c1af4196f2831d6321990fe4dea048936b444b16
TRIGGANYT <florin.gerig@icloud.com>
2025-09-17 12:54:05 +0200

    Link zusammensetzen aus GUID und Dateiendung implementert

--- Datei-Upload/frontend/js/copy-link.js
+++ Datei-Upload/frontend/js/copy-link.js
@@ -7,10 +7,18 @@ function getFileExtension(filename) {
 
 // GUID generieren
 
-function generateUUID() {
+function generateGUID() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
     const r = Math.random() * 16 | 0;
     const v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
 }
+
+// Link zusammensetzen aus GUID und Dateiendung
+
+const extension = getFileExtension(selectedFile.name);
+
+const guid = generateGUID();
+
+const downloadURL = `http://localhost:3000/files/${guid}.${extension}`;
