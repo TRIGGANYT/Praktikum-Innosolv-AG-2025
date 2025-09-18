60cbfdb321211383cb05fc843f36381217ce5e80
TRIGGANYT <florin.gerig@icloud.com>
2025-09-18 11:21:42 +0200

    file-ipload route hinzugefügt

--- Datei-Upload/backend/server.js
+++ Datei-Upload/backend/server.js
@@ -5,7 +5,14 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello, Express.js Server!</h1>');
 });
 
+// Include route files
+const fileUploadRoute = require('./routes/file-upload');
+
+// Use routes
+app.use('/file-upload', fileUploadRoute);
+
 const port = process.env.PORT || 3000; // You can use environment variables for port configuration
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
