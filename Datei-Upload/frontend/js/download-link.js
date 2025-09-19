0164de53a2e522b7b61630c26faa7359ba8716fc
TRIGGANYT <florin.gerig@icloud.com>
2025-09-19 11:59:19 +0200

    löschung unnötiger und doppelter codezeilen

--- Datei-Upload/frontend/js/download-link.js
+++ Datei-Upload/frontend/js/download-link.js
@@ -1,49 +1,10 @@
 const copyLinkBtn = document.getElementById('copy-link-btn');
 
-// Downloadlink anzeigen, sobald Datei hochladen geklickt wird
-uploadBtn.addEventListener('click', handleUploadClick);
-
-// Auf Link kopieren klicken --> Link in die Zwischenablage kopiert
-copyLinkBtn.addEventListener('click', handleCopyLinkClick);
-
 // Dateiendung extrahieren. split trennt den Dateinamen am Punkt und pop gibt den letzten Teil zurück
 function getFileExtension(filename) {
     const parts = filename.split('.');
     return parts.length > 1 ? parts.pop().toLowerCase() : '';
 }
-
-async function handleUploadClick() {
-    if (!selectedFile) {
-        alert("Bitte zuerst eine Datei auswählen.");
-        return;
-    }
-
-    const formData = new FormData();
-    formData.append("file", selectedFile); // "file" muss zum Backend passen
-
-    try {
-        const response = await fetch("http://localhost:3000/upload", {
-            method: "POST",
-            body: formData
-        });
-
-        const data = await response.json();
-
-        if (data.downloadLink) {
-            // Link anzeigen
-            document.getElementById('download-url').textContent = data.downloadLink;
-
-            // QR-Code generieren
-            generateQRCode(data.downloadLink);
-        } else if (data.error) {
-            alert("Fehler beim Hochladen: " + data.error);
-        }
-    } catch (error) {
-        console.error("Upload fehlgeschlagen:", error);
-        alert("Upload fehlgeschlagen.");
-    }
-}
-
 
 function onCopySuccess() {
     alert('Der Downloadlink wurde in die Zwischenablage kopiert.');
