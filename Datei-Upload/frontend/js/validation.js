04306f963de467f45d0de688a9013638c1de61ce
TRIGGANYT <florin.gerig@icloud.com>
2025-10-01 16:32:19 +0200

    Anzeigetext angepasst und erlaubte extensions erweitert

--- Datei-Upload/frontend/js/validation.js
+++ Datei-Upload/frontend/js/validation.js
@@ -1,6 +1,6 @@
 // validation.js
 
-const allowedExtensions = ["jpg", "jpeg", "png", "gif", "pdf", "doc", "docx", "xls", "xlsx", "txt", "pptx"];
+const allowedExtensions = ["jpg", "jpeg", "png", "gif", "pdf", "doc", "docx", "xls", "xlsx", "txt", "pptx", "odt", "rtf", "ods", "csv", "ppt", "odp", "zip"];
 
 export function isAllowedFileType(fileName) {
   const fileExtension = fileName.split('.').pop().toLowerCase();
@@ -16,7 +16,7 @@ export function validateFile(file) {
     return { valid: false, message: `${fileName}: Dateityp nicht erlaubt.` };
   }
   if (fileMb >= 50) {
-    return { valid: false, message: `${fileName}: Zu groß. Wählen Sie eine Datei kleiner als 50MB.` };
+    return { valid: false, message: `${fileName}: Zu gross. Wählen Sie eine Datei kleiner als 50MB.` };
   }
 
   return { valid: true, };
